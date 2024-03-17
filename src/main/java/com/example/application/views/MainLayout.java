115edea035bbb2137b8f3bd8d619e5084ab33e80
Chima-Anason <tblynxx@gmail.com>
2024-03-17 22:02:10 +0800

    PWA Installing offline fallback, icons

--- src/main/java/com/example/application/views/MainLayout.java
+++ src/main/java/com/example/application/views/MainLayout.java
@@ -22,7 +22,7 @@ public class MainLayout extends AppLayout {
     }
 
     private void createHeader() {
-        H1 logo = new H1("Vaadin CRM");
+        H1 logo = new H1("Vault-Tech CRM");
         logo.addClassNames("text-l", "m-m");
 
         Button logOut = new Button("Log out", e-> securityService.logout());
